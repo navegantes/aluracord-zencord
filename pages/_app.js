9205c62331595eb2db747bb36c7458d250d949dd
navegantes <raphaelnavegantes@gmail.com>
2022-01-27 23:10:40 -0300

    enable form feature

---
+++ pages/_app.js
@@ -0,0 +1,42 @@
+import appConfig from "../config.json";
+
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        background-color: ${appConfig.theme.colors.light["00"]};
+        font-family: "Open Sans", sans-serif;
+      }
+      /* App fit Height */
+      html,
+      body,
+      #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */
+    `}</style>
+  );
+}
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,9 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        background-color: ${appConfig.theme.colors.light["00"]};
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
-
 function Titulo(props) {
-  console.log(props);
   const Tag = props.tag || "h1";
 
   return (
@@ -64,19 +33,23 @@ function Titulo(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-  const username = "navegantes";
+  const [username, setUsername] = useState("");
+  const roteamento = useRouter();
+  const [isFormEnable, setIsFormEnable] = useState(false);
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+
+  useEffect(() => {
+    setIsFormEnable(username.length > 2);
+  }, [username]);
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
           backgroundColor: appConfig.theme.colors.light["00"],
-          // backgroundImage:
-          //   "url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)",
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           backgroundBlendMode: "multiply",
@@ -92,11 +65,11 @@ export default function PaginaInicial() {
               sm: "row",
             },
             width: "100%",
+            height: "35vh",
             maxWidth: "700px",
-            borderRadius: "5px",
+            borderRadius: "10px",
             padding: "32px",
             margin: "16px",
-            // boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
             boxShadow: appConfig.theme.colors.light["bs0"],
             backgroundColor: appConfig.theme.colors.light["00"],
           }}
@@ -104,17 +77,36 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
-            styleSheet={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              width: { xs: "100%", sm: "50%" },
-              textAlign: "center",
-              marginBottom: "32px",
+            onSubmit={(event) => {
+              event.preventDefault();
+              if (isFormEnable) {
+                console.log("Pagina submetida");
+                roteamento.push("/chat");
+              }
             }}
+            styleSheet={
+              !isFormEnable
+                ? {
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: { xs: "100%" },
+                    transition: "width 2s",
+                  }
+                : {
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: { xs: "100%", sm: "50%" },
+                    textAlign: "center",
+                    marginBottom: "32px",
+                    transition: "width 2s",
+                  }
+            }
           >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
+            <Titulo tag="h2">Bem-vindos ao lar</Titulo>
             <Text
               variant="body3"
               styleSheet={{
@@ -126,70 +118,124 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
-              fullWidth
+              // value={username}
+              onChange={function (event) {
+                setUsername(event.target.value);
+              }}
+              // fullWidth
               styleSheet={{
                 border: "none",
+                textAlign: "center",
+                color: appConfig.theme.colors.neutrals[300],
                 backgroundColor: "#ecfbfe",
                 boxShadow: "inset 6px 6px 10px #dbe9ec, inset -6px -6px 10px #fdffff",
                 borderRadius: "1000px",
                 padding: " 10px 20px",
                 marginBottom: "12px",
+                width: "80%",
+                // maxWidth: "60%",
               }}
             />
             <Button
               type="submit"
-              label="Entrar"
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["200"],
-                mainColor: appConfig.theme.colors.light["00"],
-                // mainColorLight: appConfig.theme.colors.neutrals[200],
-                mainColorStrong: appConfig.theme.colors.neutrals[300],
-              }}
-              styleSheet={{
-                borderRadius: "1000px",
-                boxShadow: appConfig.theme.colors.light["bs0"],
-              }}
+              label={!isFormEnable ? "" : "Entrar"}
+              disabled={!isFormEnable}
+              // fullWidth
+              buttonColors={
+                !isFormEnable
+                  ? {
+                      mainColor: appConfig.theme.colors.light["00"],
+                    }
+                  : {
+                      contrastColor: appConfig.theme.colors.neutrals["200"],
+                      mainColor: appConfig.theme.colors.light["00"],
+                      // mainColorLight: appConfig.theme.colors.neutrals[200],
+                      mainColorStrong: appConfig.theme.colors.light["00"],
+                    }
+              }
+              styleSheet={
+                !isFormEnable
+                  ? {
+                      borderRadius: "1000px",
+                      boxShadow: "none",
+                      padding: "15px 0px 15px",
+                      transition: "box-shadow 2s",
+                      // border: "1px solide black",
+                    }
+                  : {
+                      width: "80%",
+                      borderRadius: "1000px",
+                      boxShadow: appConfig.theme.colors.light["bs0"],
+                      transition: "box-shadow 2s",
+                      hover: {
+                        boxShadow: appConfig.theme.colors.light["bs00"],
+                      },
+                      focus: {
+                        boxShadow: appConfig.theme.colors.light["bs00"],
+                      },
+                    }
+              }
             />
           </Box>
           {/* Formulário */}
 
           {/* Photo Area */}
           <Box
-            styleSheet={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              maxWidth: "200px",
-              padding: "16px",
-              backgroundColor: appConfig.theme.colors.light["00"],
-              boxShadow: appConfig.theme.colors.light["bs1"],
-              // border: "1px solid",
-              // borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: "10px",
-              flex: 1,
-              minHeight: "240px",
-            }}
+            styleSheet={
+              !isFormEnable
+                ? { boxShadow: "none", transition: "box-shadow 2s" }
+                : {
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    maxWidth: "200px",
+                    padding: "16px",
+                    backgroundColor: appConfig.theme.colors.light["00"],
+                    boxShadow: appConfig.theme.colors.light["bs1"],
+                    borderRadius: "10px",
+                    flex: 1,
+                    minHeight: "240px",
+                    transition: "box-shadow 1s, max-width 2s",
+                  }
+            }
           >
             <Image
-              styleSheet={{
-                borderRadius: "50%",
-                marginBottom: "16px",
-                // boxShadow: appConfig.theme.colors.light["bs0"],
-              }}
-              src={`https://github.com/${username}.png`}
+              styleSheet={
+                !isFormEnable
+                  ? {
+                      boxShadow: "none",
+                      opacity: "0",
+                      transition: "box-shadow 2s, opacity 2s",
+                    }
+                  : {
+                      borderRadius: "50%",
+                      marginBottom: "20px",
+                      maxWidth: "90%",
+                      boxShadow: appConfig.theme.colors.light["bs00"],
+                      opacity: "1",
+                      transition: "box-shadow 2s, opacity 3s",
+                    }
+              }
+              src={!isFormEnable ? "" : `https://github.com/${username}.png`}
             />
-            <Text
-              variant="body4"
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals[100],
-                backgroundColor: appConfig.theme.colors.neutrals[300],
-                padding: "3px 10px",
-                borderRadius: "1000px",
-              }}
-            >
-              {username}
-            </Text>
+            {!isFormEnable ? (
+              ""
+            ) : (
+              <Text
+                variant="body4"
+                styleSheet={{
+                  fontSize: "0.9rem",
+                  color: appConfig.theme.colors.neutrals[300],
+                  backgroundColor: appConfig.theme.colors.light["00"],
+                  boxShadow: appConfig.theme.colors.light["bs00"],
+                  padding: "3px 10px",
+                  borderRadius: "1000px",
+                  transition: "box-shadow 2.5s",
+                }}
+              >
+                {username}
+              </Text>
+            )}
           </Box>
           {/* Photo Area */}
         </Box>
